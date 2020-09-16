a1e23aebf8ab5a3e921855aaeb2cbc076ccd876b
Q00 <jqyu.lee@gmail.com>
2020-09-16 23:02:24 +0900

    add solution

--- array/array7.go
+++ array/array7.go
@@ -23,3 +23,15 @@ func plusOne(digits []int) []int {
 	fmt.Println(digits)
 	return digits
 }
+
+func plusOne2(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	digits = append([]int{1}, digits...)
+	return digits
+}
--- array/array9.go
+++ array/array9.go
@@ -1,19 +1,18 @@
 package array
 
 func twoSum(nums []int, target int) []int {
-	check := map[int][]int{}
-
+	check := map[int]int{}
+	// [3,3]
 	for i, v := range nums {
-		check[v] = append(check[v], i)
 
-		if len(check[target-v]) >= 1 {
-			if v == target-v && len(check[v]) > 1 {
-				return check[v]
-			} else if v != target-v {
-				return []int{check[target-v][0], check[v][0]}
-			}
+		gap := target - v
+		value, isExist := check[gap]
+		if isExist {
+			return []int{value, i}
+		} else {
+			check[v] = i
 		}
-	}
 
+	}
 	return nums
 }
